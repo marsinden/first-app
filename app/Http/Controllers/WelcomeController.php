935124daf16e20d7aeeac96f82bd812aa3277eb3
Denys Synytsia <chagery@gmail.com>
2023-04-18 20:35:32 +0200

    Working with the database

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     public function index() {
-        return view('welcome');
+        // $users = DB::select('select * from users');
+        // dd($users);
+        $users = DB::table('users')->select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+        dd($users);
     }
 }
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,4 +4,24 @@
 
 @section('content')
 <h1 class="text-4xl font-bold">About</h1>
+<h2>Rock Paper Scissors</h2>
+<?php
+if ( isset($_REQUEST['name']) ) {
+    echo "<p>Welcome: ";
+    echo htmlentities($_REQUEST['name']);
+    echo "</p>\n";
+}
+
+?>
+<form method="post">
+<select name="human">
+<option value="-1">Select</option>
+<option value="0">Rock</option>
+<option value="1">Paper</option>
+<option value="2">Scissors</option>
+<option value="3">Test</option>
+</select>
+<input type="submit" value="Play">
+<input type="submit" name="logout" value="Logout">
+</form>
 @endsection
